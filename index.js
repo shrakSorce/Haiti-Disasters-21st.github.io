b75b4954c39ee7118f915d5941ac12ee78ada28d
shrakSorce <113307071+shrakSorce@users.noreply.github.com>
2023-02-17 04:22:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let cursor = document.querySelector('.cursor');
-
-document.addEventListener('mousemove', moveCursor)
-
-function moveCursor(e){
-    let x = e.clientX
-    let y = e.clientY
-
-
-cursor.style.left = `${x}px`;
-cursor.style.top = `${y}px`;
-}
 
